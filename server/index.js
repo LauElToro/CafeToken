873d0c6266a1411b9d5b814cadbe3827d6523b21
Lautaro Figueroa <LautyfigueroaLau@gmail.com>
2023-08-08 18:31:51 -0300

    CofeToken0.0.0.01

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import Stripe from "stripe";
+import cors from "cors";
+
+const stripe = new Stripe("<your_secretkey_here>");
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
+
+app.post("/api/checkout", async (req, res) => {
+  const { id, amount } = req.body;
+
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Gaming Keyboard",
+      payment_method: id,
+      confirm: true,
+    });
+
+    console.log(payment);
+
+    return res.status(200).json({ message: "Successful Payment" });
+  } catch (error) {
+    console.log(error);
+    return res.json({ message: error.raw.message });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server on port", 3001);
+});
